9053710da47686fa925502e02d66684b6287efd1
Christos Georgiou <xris.georgiou@gmail.com>
2015-03-05 01:20:24 +0200

    Main landing segment

--- src/Geo/AppBundle/Controller/MainController.php
+++ src/Geo/AppBundle/Controller/MainController.php
@@ -8,49 +8,32 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class MainController extends Controller {
+class MainController extends Controller
+{
 
     /**
      * @Route("/", name="Dashboard")
      * @Template()
      */
-    public function mainAction() {
+    public function mainAction()
+    {
+        $parties = $this->getDoctrine()->getRepository("GeoAppBundle:Party")->findBy(array(), array('createdAt' => 'DESC'));
 
-      $greetings=array(
-        "3" => "Go to bed...",
-        "6" => "Mornin' Sunshine",
-        "12" => "Good morning",
-        "14" => "Good day",
-        "17" => "Hello!!",
-        "19" => "Good afternoon",
-        "21" => "Good evening",
-        "23" => "Go to bed...",
-      );
-
-      foreach($greetings as $_time=>$_msg){
-        if(date("G")<=$_time)
-        {
-          $msg = $_msg;//"{$_msg}.{$_time}".date("G");
-          break;
-        }
-      }
-
-      $parties = $this->getDoctrine()->getRepository("GeoAppBundle:Party")->findBy(array(), array('createdAt' => 'DESC'));
-
-      return array(
-        "parties" => $parties,
-        "greeting" => $msg,
-      );
+        return array(
+            "parties" => $parties,
+            "greeting" => "Hello!!",
+        );
     }
 
     /**
      * @Route("/party/{id}", name="Party")
      * @Template()
      */
-    public function partyAction($id) {
+    public function partyAction($id)
+    {
         $party = $this->getDoctrine()->getRepository("GeoAppBundle:Party")->findOneById($id);
         return array(
-          "party" => $party,
+            "party" => $party,
         );
     }
 
