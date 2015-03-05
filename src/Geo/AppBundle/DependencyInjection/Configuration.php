17c7331eea9182160981ffaa7bc8509178ce0e7e
Christos Georgiou <xris.georgiou@gmail.com>
2015-03-05 22:53:35 +0200

    Trying to create a twig extension
    failed... T_T

---
+++ src/Geo/AppBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Geo\AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('geo_test');
+        $rootNode
+            ->children()
+            ->scalarNode('app_name')->end()
+            ->scalarNode('app_ver')->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
